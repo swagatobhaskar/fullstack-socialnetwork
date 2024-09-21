3fd03322060a057a4d7e94a7dfafc6b361979de8
swagatobhaskar <bhaskarswagato@gmail.com>
2024-09-21 23:11:30 +0530

    removed git markings from code

--- backend/app/models/user_account_model.py
+++ backend/app/models/user_account_model.py
@@ -1,9 +1,4 @@
-from flask_sqlalchemy import SQLAlchemy
-<<<<<<< HEAD
-from sqlalchemy .sql import func
-=======
 from sqlalchemy.sql import func
->>>>>>> user_content_backend
 from werkzeug.security import generate_password_hash, check_password_hash
 import enum
 
